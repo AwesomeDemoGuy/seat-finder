58f9290d58449e16654870d9087932e6450ec4b2
AwesomeDemoGuy <sebastian11ashkar@protonmail.com>
2024-11-20 23:53:42 -0700

    updated webhook URL handeling

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 import requests
-
-URL = "https://discord.com/api/webhooks/1309044790821785660/Nb_VWUae-weMRtGGk3Kc_YheYjQ4zpUrdlqkKlHPtdXaWNKlybQLUZgurNt1v9BnnscU"
+import os
+# Grab the url from the .env file
+URL = os.getenv("URL")
 
 def notify(class_name, current_value, url):
   requests.post(URL, json={ "content": f"<@180423023340290048> {class_name}: Seats open: {current_value}\n{url}"})
